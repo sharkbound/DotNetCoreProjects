717f9c5b32f86180fa9b0c9f3e62e7d956cd2dfe
sharkbound <laptopblaster@gmail.com>
2016-08-18 15:36:23 -0700

    added missing file XD

---
+++ TestingProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Dir = @"\TextFile";
+            Console.WriteLine("Hello World!");
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\TextFile");
+            File.CreateText(Directory.GetCurrentDirectory() + @"\TextFile\text.txt");
+            if (File.Exists(Dir))
+            {
+                Console.WriteLine("File Found!");
+            }
+            Console.ReadKey();
+            Console.WriteLine("Done...");
+        }
+    }
+}
